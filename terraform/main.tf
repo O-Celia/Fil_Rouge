13201c553dd3c1724abf0a2c320d9d434ab32832
celia <celia.ouedrao@gmail.com>
2023-10-22 20:56:11 +0200

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,31 @@ resource "azurerm_resource_group" "aks" {
   location = var.location
 }
 
+/* resource "azurerm_mysql_flexible_server" "mysql" {
+  name                = "mysql-server-celia"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  administrator_login =  "wordpress"
+  administrator_password = "Witcher_95"
+  sku_name            = "GP_Standard_D2ads_v5"
+}
+
+resource "azurerm_mysql_flexible_database" "database" {
+  name                = "mysql-database-celia"
+  resource_group_name = azurerm_resource_group.aks.name
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  charset             = "utf8"
+  collation           = "utf8_general_ci"
+} */
+/* 
+resource "azurerm_mysql_flexible_server_firewall_rule" "traefik_firewall_rule" {
+  name                = "allow-traefik"
+  resource_group_name = azurerm_mysql_flexible_server.mysql.resource_group_name
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  start_ip_address    = "51.145.181.57"
+  end_ip_address      = "51.145.181.57"
+} */
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_cluster_name
   location            = azurerm_resource_group.aks.location
@@ -24,13 +49,4 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
-}
 
