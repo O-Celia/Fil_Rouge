047eb67a4a7b54f673ca6396f1e212e678b9159a
celia <celia.ouedrao@gmail.com>
2023-11-23 15:34:41 +0100

    remove tls

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,22 +3,22 @@ resource "azurerm_resource_group" "aks" {
   location = var.location
 }
 
-resource "azurerm_mysql_flexible_server" "mysql" {
-  name                = "mysql-server-celia"
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
-  administrator_login =  "wordpress"
-  administrator_password = "Witcher_95"
-  sku_name            = "GP_Standard_D2ads_v5"
-}
+# resource "azurerm_mysql_flexible_server" "mysql" {
+#   name                = "mysql-server-celia"
+#   location            = azurerm_resource_group.aks.location
+#   resource_group_name = azurerm_resource_group.aks.name
+#   administrator_login =  "wordpress"
+#   administrator_password = "Witcher_95"
+#   sku_name            = "GP_Standard_D2ads_v5"
+# }
 
-resource "azurerm_mysql_flexible_database" "database" {
-  name                = "mysql-database-celia"
-  resource_group_name = azurerm_resource_group.aks.name
-  server_name         = azurerm_mysql_flexible_server.mysql.name
-  charset             = "utf8"
-  collation           = "utf8_general_ci"
-}
+# resource "azurerm_mysql_flexible_database" "database" {
+#   name                = "mysql-database-celia"
+#   resource_group_name = azurerm_resource_group.aks.name
+#   server_name         = azurerm_mysql_flexible_server.mysql.name
+#   charset             = "utf8"
+#   collation           = "utf8_general_ci"
+# }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_cluster_name
