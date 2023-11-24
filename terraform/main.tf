07d54f116cd096b177f6aab1ee9c19d0a377b033
celia <celia.ouedrao@gmail.com>
2023-11-24 11:15:55 +0100

    change mysql

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "aks" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet_mysql"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
   address_space       = ["10.0.0.0/16"]
 }
 
